8c30e16816a90f191c838a6d85e341755499c335
zelazunio <ja_zel@wp.pl>
2022-08-29 16:56:17 +0200

    Final commit

--- src/App.vue
+++ src/App.vue
@@ -6,18 +6,26 @@
         class="h-75 align-self-center w-100 contentContainer"
       >
         <b-col class="justify-content-center">
+          <b-form-input v-model="nameSearch"
+            placeholder="Name filter"
+            class="m-2 w-50"
+          />
           <todo-renderer
-            v-for="todo in todos"
+            v-for="todo in filteredTodos"
             :key="todo.id"
             :todo="todo"
+            :names="owners"
             @discardEdition="discardEdition"
-            @saveEditedTodo="saveTodo"
+            @saveEditedTodo="saveEditedTodo"
+            @deleteTodo="showDeleteTodoModal"
           />
           <todo-renderer
             :todo="newTodo"
+            :names="owners"
             force-edition
             v-if="addNewTodo"
             @discardEdition="cancelAddingNewTodo"
+            @saveEditedTodo="addTodo"
           />
           <div class="container-fluid" v-if="!addNewTodo">
             <b-row class="align-self-center w-100">
@@ -31,12 +39,23 @@
         </b-col>
       </b-row>
     </b-container>
+    <b-modal v-model="showDeleteModal">
+    <template v-slot:modal-header>
+      Delete ToDo
+    </template>
+      Confirm operation
+      <template v-slot:modal-footer>
+        <b-btn variant="primary" @click="deleteTodo">OK</b-btn>
+        <b-btn @click="showDeleteModal=false">Cancel</b-btn>
+      </template>
+    </b-modal>
   </div>
 </template>
 
 <script>
 import todoRenderer from "./components/todoRenderer.vue";
 import TodoRenderer from "./components/todoRenderer.vue";
+import Vue from "vue";
 
 export default {
   name: "App",
@@ -46,13 +65,20 @@ export default {
   },
   data() {
     return {
-      todos: {
-        1: { id: 1, owner: "Mark", text: "Kupić ogóry", done: false },
-        2: { id: 2, owner: "Mark", text: "Kupić papier", done: false },
-      },
+      todos: {},
       newTodo: { owner: "", text: "" },
       addNewTodo: false,
+      showDeleteModal: false,
+      todoToDeleteId: null,
+      nameSearch:''
     };
+  },
+  created() {
+    try {
+      this.todos = {...this.todos, ...JSON.parse(localStorage.getItem("MyTodos"))};
+    } catch (e) {
+      console.debug(e);
+    }
   },
   methods: {
     activateAddNewTodo() {
@@ -63,11 +89,51 @@ export default {
       this.todos[params.todo.id].owner = params.rememberedOwner;
       this.todos[params.todo.id].text = params.rememberedText;
     },
-    saveTodo() {},
+    saveEditedTodo(params) {
+      let newTodo = { id: params.id, owner: params.owner, text: params.text };
+      Vue.set(this.todos, params.id, newTodo);
+      this.saveTodos();
+      this.addNewTodo = false;
+    },
+    addTodo(params) {
+      let newTodoId = Math.max(...[0, ...Object.keys(this.todos)]) + 1;
+      let newTodo = { id: newTodoId, owner: params.owner, text: params.text };
+      Vue.set(this.todos, newTodoId, newTodo);
+      this.saveTodos();
+      this.addNewTodo = false;
+    },
+    saveTodos() {
+      try {
+        localStorage.setItem("MyTodos", JSON.stringify(this.todos));
+      } catch (e) {
+        console.debug(e);
+      }
+    },
     cancelAddingNewTodo() {
       this.addNewTodo = false;
     },
+    showDeleteTodoModal(params) {
+      this.todoToDeleteId = params.id;
+      this.showDeleteModal = true;
+    },
+    deleteTodo() {
+      Vue.delete(this.todos, this.todoToDeleteId);
+      this.saveTodos();
+      this.showDeleteModal = false;
+    }
   },
+  computed: {
+    owners() {
+      return Array.from(new Set(Object.values(this.todos).map(todo=>(todo.owner))));
+    },
+    filteredTodos() {
+      let result = {};
+      Object.values(this.todos).forEach(todo => {
+       if (todo.owner.toLowerCase().includes(this.nameSearch.toLowerCase())) result[todo.id] = todo;
+      })
+      return result;
+    }
+  }
 };
 </script>
 
--- src/components/todoRenderer.vue
+++ src/components/todoRenderer.vue
@@ -5,11 +5,13 @@
         <div class="row">
           <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 col-xl-4 d-flex">
             <b-form-input
+              list="namesList"
               v-model="todo.owner"
               type="text"
               :disabled="!editionActive"
               :placeholder="this.ownerPlaceholder"
             />
+            <b-form-datalist id="namesList" :options="names" />
           </div>
           <div class="col-xs-12 col-sm-12 col-md-8 col-lg-8 col-xl-8">
             <b-form-input
@@ -30,16 +32,19 @@
         >
           <b-icon-pencil-fill></b-icon-pencil-fill>
         </b-btn>
+        <b-btn pill variant="danger" v-if="!editionActive" @click="deleteTodo">
+          <b-icon-x-square-fill></b-icon-x-square-fill>
+        </b-btn>
         <b-btn
           pill
-          variant="danger"
+          variant="warning"
           class="mr-1"
           v-if="editionActive"
           @click="discardEdition"
         >
-          <b-icon-x-square-fill></b-icon-x-square-fill>
+          <b-icon-backspace-fill></b-icon-backspace-fill>
         </b-btn>
-        <b-btn pill variant="success" v-if="editionActive">
+        <b-btn pill variant="success" v-if="editionActive" @click="saveData">
           <b-icon-check-square-fill></b-icon-check-square-fill>
         </b-btn>
       </div>
@@ -59,9 +64,10 @@ export default {
   },
   props: {
     todo: { type: Object, required: true },
-    forceEdition: {type: Boolean, default: false},
-    ownerPlaceholder: {type: String, default: "Kto"},
-    textPlaceholder: {type: String, default: "Co zrobić"},
+    forceEdition: { type: Boolean, default: false },
+    ownerPlaceholder: { type: String, default: "Kto" },
+    textPlaceholder: { type: String, default: "Co zrobić" },
+    names: { type: Array, default: () => [] },
   },
   mounted() {
     if (this.forceEdition) this.activateEdition();
@@ -74,11 +80,22 @@ export default {
     },
     saveData() {
       this.editionActive = false;
-      this.$emit('saveEditedTodo', {owner: this.todo.owner, text: this.todo.text})
+      this.$emit("saveEditedTodo", {
+        id: this.todo.id,
+        owner: this.todo.owner,
+        text: this.todo.text,
+      });
     },
     discardEdition() {
       this.editionActive = false;
-      this.$emit('discardEdition', {todo: this.todo, rememberedOwner: this.rememberedOwner, rememberedText: this.rememberedText})
+      this.$emit("discardEdition", {
+        todo: this.todo,
+        rememberedOwner: this.rememberedOwner,
+        rememberedText: this.rememberedText,
+      });
+    },
+    deleteTodo() {
+      this.$emit("deleteTodo", { id: this.todo.id });
     },
   },
 };
